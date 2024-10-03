a047a35baaa3d2c841e1b7951dd5d5f5783f7470
Md. Mehedi Hassan <mh.web.mehedihassan@gmail.com>
2024-10-03 18:59:01 +0600

    EC-00: Refactor App component to use Material-UI components and update state handling

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,24 +1,16 @@
-import { useState } from "react";
+import { Box, Button, Typography } from '@mui/material';
+import { useState } from 'react';
 
 function App() {
-    const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
 
-    return (
-        <>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
-        </>
-    );
+  return (
+    <Box>
+      <Typography variant="h1">{count}</Typography>
+      <Button onClick={() => setCount(count + 1)}>Increment</Button>
+      <Button onClick={() => setCount(count - 1)}>Decrement</Button>
+    </Box>
+  );
 }
 
 export default App;
