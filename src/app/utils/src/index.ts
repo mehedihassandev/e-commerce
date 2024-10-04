526db9a9e2ceff5d5f5e8ce0f904c65562afdd6d
Md. Mehedi Hassan <mh.web.mehedihassan@gmail.com>
2024-10-04 14:35:25 +0600

    EC-02: Move hook to lib file

--- src/app/utils/src/index.ts
+++ src/app/utils/src/index.ts
@@ -1,5 +1,5 @@
-export * from './hooks/network-detector/network-detector';
-export * from './hooks/notification-hook/notification-hook';
 export * from './lib/content-wrapper/content-wrapper';
+export * from './lib/hooks/network-detector/network-detector';
+export * from './lib/hooks/notification-hook/notification-hook';
 export * from './lib/icon-hash/icon-hash';
 export * from './lib/rhf-textfield/rhf-text-field';
