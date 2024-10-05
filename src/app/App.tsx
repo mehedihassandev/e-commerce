f6326e113295f634fe79498ca6883e9d74b4895d
Md. Mehedi Hassan <mh.web.mehedihassan@gmail.com>
2024-10-05 13:50:31 +0600

    EX-04: Refactor package.json to add @tanstack/react-query and axios dependencies

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,21 +1,26 @@
 import { CssBaseline } from '@mui/material';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter } from 'react-router-dom';
 import { RouterConfig } from './navigation/RouterConfig';
 import { NetworkDetector, NotificationProvider } from './utils/src';
 import { ThemeProviderWrapper } from './utils/src/lib/theme';
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <NetworkDetector>
-      <BrowserRouter>
-        <ThemeProviderWrapper>
-          <NotificationProvider>
-            <CssBaseline />
-            <RouterConfig />
-          </NotificationProvider>
-        </ThemeProviderWrapper>
-      </BrowserRouter>
-    </NetworkDetector>
+    <QueryClientProvider client={queryClient}>
+      <NetworkDetector>
+        <BrowserRouter>
+          <ThemeProviderWrapper>
+            <NotificationProvider>
+              <CssBaseline />
+              <RouterConfig />
+            </NotificationProvider>
+          </ThemeProviderWrapper>
+        </BrowserRouter>
+      </NetworkDetector>
+    </QueryClientProvider>
   );
 }
 
