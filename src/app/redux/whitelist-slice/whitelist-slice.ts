1c54565f7fb45c4626c22ec6dbfa909d7d407a85
Md. Mehedi Hassan <mh.web.mehedihassan@gmail.com>
2024-10-05 15:02:42 +0600

    EC-04: Refactor whitelist-slice.ts to use interface name convention

--- src/app/redux/whitelist-slice/whitelist-slice.ts
+++ src/app/redux/whitelist-slice/whitelist-slice.ts
@@ -1,10 +1,10 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
-interface WhitelistState {
+interface IWhitelistState {
   whitelistedProducts: number[];
 }
 
-const initialState: WhitelistState = {
+const initialState: IWhitelistState = {
   whitelistedProducts: []
 };
 
