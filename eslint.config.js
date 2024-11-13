f26b38b8a63ffd13f9827bd2fc7087dd975a551c
Md. Mehedi Hassan <mh.web.mehedihassan@gmail.com>
2024-11-13 16:56:32 +0600

    build-issue: Refactor checkout form and product details pages to replace RhfTextField with MUI TextField; remove RhfTextField component for build issue

--- eslint.config.js
+++ eslint.config.js
@@ -1,44 +1,49 @@
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import typescriptParser from '@typescript-eslint/parser';
+import importPlugin from 'eslint-plugin-import';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
+import prettier from 'eslint-plugin-prettier';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import unusedImports from 'eslint-plugin-unused-imports';
+
 const config = [
   {
     files: ['**/*.ts', '**/*.tsx'],
-    // languageOptions: {
-    //   parser: typescriptParser,
-    //   parserOptions: {
-    //     ecmaFeatures: {
-    //       jsx: true
-    //     },
-    //     ecmaVersion: 'latest',
-    //     sourceType: 'module'
-    //   },
-    //   globals: {
-    //     browser: true,
-    //     es2021: true
-    //   }
-    // },
-    // plugins: {
-    //   react,
-    //   'react-hooks': reactHooks,
-    //   import: importPlugin,
-    //   'unused-imports': unusedImports,
-    //   'jsx-a11y': jsxA11y,
-    //   '@typescript-eslint': typescriptEslint,
-    //   'simple-import-sort': simpleImportSort,
-    //   prettier,
-    //   'react-refresh': reactRefresh
-    // },
+    languageOptions: {
+      parser: typescriptParser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true
+        },
+        ecmaVersion: 'latest',
+        sourceType: 'module'
+      },
+      globals: {
+        browser: true,
+        es2021: true
+      }
+    },
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      import: importPlugin,
+      'unused-imports': unusedImports,
+      'jsx-a11y': jsxA11y,
+      '@typescript-eslint': typescriptEslint,
+      'simple-import-sort': simpleImportSort,
+      prettier,
+      'react-refresh': reactRefresh
+    },
     rules: 'off',
     // settings: {
     //   react: {
     //     version: 'detect'
     //   }
     // },
-    ignores: [
-      'dist',
-      'build',
-      'node_modules/',
-      '.eslintrc.json',
-      'src/app/utils/src/lib/rhf-textfield/index.tsx'
-    ]
+    ignores: ['dist', 'build', 'node_modules/', '.eslintrc.json']
   }
 ];
 
--- src/app/utils/src/index.ts
+++ src/app/utils/src/index.ts
@@ -2,4 +2,3 @@ export * from './lib/content-wrapper/content-wrapper';
 export * from './lib/hooks/notification-hook/notification-hook';
 export * from './lib/icon-hash/icon-hash';
 export * from './lib/rhf-select';
-export * from './lib/rhf-textfield';
